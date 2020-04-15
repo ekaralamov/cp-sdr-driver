ca7811c58a2a6053d9a0495c02e35cdf18bf0926
Evgeni Karalamov <ekaralamov@gmail.com>
2020-04-15 14:17:31 +0300

    Extract client lib plug-in

---
+++ buildSrc/src/main/kotlin/app/ekaralamov/gradle/ClientLibPlugin.kt
@@ -0,0 +1,35 @@
+package app.ekaralamov.gradle
+
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.get
+
+class BaseClientLibPlugin : Plugin<Project> {
+    override fun apply(project: Project) = with(project) {
+        apply(plugin = "com.android.library")
+        apply(plugin = "kotlin-android")
+
+        configure<BaseExtension> {
+            setCompileSdkVersion(versionOf<Int>("CompileSdk"))
+
+            defaultConfig {
+                setMinSdkVersion(1)
+                setTargetSdkVersion(versionOf<Int>("CompileSdk"))
+            }
+        }
+
+        dependencies {
+            with(configurations["implementation"]) {
+                invoke(
+                    group = "org.jetbrains.kotlin",
+                    name = "kotlin-stdlib",
+                    version = versionOf("Kotlin")
+                )
+            }
+        }
+    }
+}
