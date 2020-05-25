a536ef32430e5a146d253eb9ae68610bededab0f
Evgeni Karalamov <ekaralamov@gmail.com>
2020-05-25 12:40:54 +0300

    Make the app apply for persistent device permission

---
+++ permissions/shell/src/main/java/app/ekaralamov/sdr/driver/permissions/DummyActivity.kt
@@ -0,0 +1,12 @@
+package app.ekaralamov.sdr.driver.permissions
+
+import android.app.Activity
+import android.os.Bundle
+
+class DummyActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
