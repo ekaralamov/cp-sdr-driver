b1a5533070292808c22b23c902bfb4f8631a3944
Evgeni Karalamov <ekaralamov@gmail.com>
2020-06-05 01:22:49 +0300

    Add TunerAccessTokenStressTest

---
+++ common/operations/src/test/java/app/ekaralamov/sdr/driver/TunerAccessTokenStressTest.kt
@@ -0,0 +1,102 @@
+package app.ekaralamov.sdr.driver
+
+import app.ekaralamov.test.FailOnThreadExceptionRule
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
+import org.junit.Test
+import java.util.concurrent.ThreadLocalRandom
+import java.util.concurrent.atomic.AtomicBoolean
+
+class TunerAccessTokenStressTest {
+
+    @get:Rule
+    val failOnThreadExceptionRule = FailOnThreadExceptionRule()
+
+    private class Session : TunerAccessToken.Session {
+
+        private val open = AtomicBoolean()
+
+        fun open() {
+            assertThat(open.compareAndSet(false, true)).isTrue()
+        }
+
+        override suspend fun close() {
+            assertThat(open.compareAndSet(true, false)).isTrue()
+        }
+    }
+
+    @Test
+    fun test() {
+        val threadCount = 150
+
+        val sessionFactory : (Session) -> Session = { it.apply { open() } }
+
+        val sessions = arrayOf(Session(), Session())
+        val packageNames = arrayOf("package 1", "package 2")
+
+        val permissionStorage = mockk<ClientPermissionStorage> {
+            coEvery { retrieveResolutionFor("package 1") } returns ClientPermissionResolution.Permanent.Granted
+            coEvery { retrieveResolutionFor("package 2") } returns ClientPermissionResolution.Permanent.Granted
+            coEvery { retrieveResolutionFor("package 3") } returns null
+        }
+
+        val sut = TunerAccessToken.Registry<Session, Session>(permissionStorage)
+
+        val random = ThreadLocalRandom.current()
+
+        class AuthorizedClient : Runnable {
+
+            private val packageName = packageNames[random.nextInt(2)]
+            private val session = sessions[random.nextInt(2)]
+
+            override fun run() {
+                try {
+                    val token = sut.acquireToken(session, packageName, sessionFactory)
+                    assertThat(token.session).isEqualTo(session)
+                    runBlocking { token.release() }
+                } catch (deviceBusyException: DeviceBusyException) {
+                } catch (permissionRevokedException: PermissionRevokedException) {
+                }
+            }
+        }
+
+        class UnauthorizedClient : Runnable {
+
+            private val session = sessions[random.nextInt(2)]
+
+            override fun run() {
+                try {
+                    sut.acquireToken(session, "package 3", sessionFactory)
+                    throw AssertionError("should have failed")
+                } catch (deviceBusyException: DeviceBusyException) {
+                } catch (securityException: SecurityException) {
+                }
+            }
+        }
+
+        class Revoker : Runnable {
+
+            private val packageName = packageNames[random.nextInt(2)]
+
+            override fun run() {
+                runBlocking { sut.revokeTokensFor(packageName) }
+            }
+        }
+
+        val threads = Array(threadCount) {
+            Thread(
+                when (random.nextInt(8)) {
+                    in 0..5 -> AuthorizedClient()
+                    6 -> UnauthorizedClient()
+                    7 -> Revoker()
+                    else -> throw AssertionError()
+                }
+            ).apply { start() }
+        }
+
+        threads.forEach { it.join() }
+    }
+}
